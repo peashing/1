4d857d7df3768ce0493287670f0c649bce5c6819
Ivan Filyaev <filyaev.ip@gmail.com>
2017-06-28 15:38:10 +0600

    hw2.2

---
+++ hw2.2.py
@@ -0,0 +1,38 @@
+#menu = readreciept('C:/Python36/file.txt')
+#with open('C:/Python36/file_json.json', 'wt') as f:
+	#f.write(json.dumps(menu, ensure_ascii=False, indent=4, sort_keys=True))
+
+def read_from_json(jsonfile):
+    import json
+    menu = {}
+    with open(jsonfile, 'rt') as f:
+        menu = dict(json.loads(f.read()))
+    return menu
+
+
+cook_book = read_from_json('C:/Python36/file_json.json')
+
+def get_shop_list_by_dishes(dishes, person_count):
+  shop_list = {}
+  for dish in dishes:
+    for ingridient in cook_book[dish]:
+      new_shop_list_item = dict(ingridient)
+      new_shop_list_item['quantity'] *= person_count
+      if new_shop_list_item['ingridient_name'] not in shop_list:
+        shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
+      else:
+        shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
+  return shop_list
+
+def print_shop_list(shop_list):
+  for shop_list_item in shop_list.values():
+    print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'],
+                            shop_list_item['measure']))
+def create_shop_list():
+  person_count = int(input('Введите количество человек: '))
+  dishes = input('Введите блюда в расчете на одного человека (через запятую): ') \
+    .lower().split(', ')
+  shop_list = get_shop_list_by_dishes(dishes, person_count)
+  print_shop_list(shop_list)
+
+create_shop_list()
